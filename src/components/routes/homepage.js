cfbbd12480c7d315028c0a3eb50830a8bedbfdc1
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2023-04-05 19:08:23 -0500

    feat: source data from json

--- src/components/routes/homepage.js
+++ src/components/routes/homepage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import block from "../../data/church.json";
 
 export default function HomePage( props ) {
     return(
         <>
             <section>
-                <h1>AMAZING GRACE MINISTRIES</h1>
+                <h1>{ block.name }</h1>
             </section>
         </>
     )
