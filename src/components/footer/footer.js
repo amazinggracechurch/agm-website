83ac27acc379f500353162d708c2c86752b3ff76
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2023-04-09 14:58:32 -0500

    feat: footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import links from '../../data/links.json';
 
 export default function footer() {
   return (
     <>
         <div className="half-section">
             <div className="base">
+              <a href="aggospel.net">{ links[ 0 ].url }</a>
             </div>
         </div>
     </>
