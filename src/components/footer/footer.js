6434d768336f58ae75f88c2480852fed2c741f45
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2023-04-12 19:25:02 -0500

    feat: some updates to the footer to map links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import links from '../../data/links.json';
+import './footer.css';
 
 export default function footer() {
   return (
     <>
-        <div className="half-section">
-            <div className="base">
+      <div className="half-section">
+          <div className="base">
+            <div className="grid">
               <a href="aggospel.net">{ links[ 0 ].url }</a>
             </div>
+            <div className="grid">
+                <h3>Additional Links</h3>
+                {links.map( ( { url, type } ) => (
+                  <a href={ url } key={ type }>
+                    { type }
+                  </a>
+                ) )}
+            </div>
+          </div>
         </div>
     </>
   );
