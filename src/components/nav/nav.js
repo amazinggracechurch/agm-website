3ff3cb433bf2113176d04234988eccc10aeadf0b
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2023-12-27 21:42:07 -0600

    feat: updates to nav and styles

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,12 +5,12 @@ export default function nav() {
   return (
     <div className="nav">
         <ul>
-            <li><p><button href="#">About</button></p></li>
+            <li><p><button href="#">About us</button></p></li>
             <li><p><button href="#">Ministries</button></p></li>
-        </ul>
-        <ul>
+            <li><p><button href="#">Home</button></p></li>
+            <li><p><button href="#">Livestream</button></p></li>
             <li><p><button href="#">Resources</button></p></li>
-            <li><p><button href="#">Give</button></p></li>
+            <li><p><button href="#">Donate</button></p></li>
         </ul>
     </div>
   );
