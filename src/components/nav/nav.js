4190f8c841e11735ecb8b800fa09e49508738075
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2024-01-10 13:21:01 -0600

    fix: nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,12 +5,13 @@ export default function nav() {
   return (
     <div className="nav">
         <ul>
-            <li><p><button href="#">About</button></p></li>
-            <li><p><button href="#">Ministries</button></p></li>
             <li><p><button href="#">Home</button></p></li>
+            <li><p><button href="#">About Us</button></p></li>
+            <li><p><button href="#">Ministries</button></p></li>
             <li><p><button href="#">Livestream</button></p></li>
             <li><p><button href="#">Resources</button></p></li>
             <li><p><button href="#">Donate</button></p></li>
+            <li><p><button href="#">Contact Us</button></p></li>
         </ul>
     </div>
   );
