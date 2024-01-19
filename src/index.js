96c74f281c4722e167c6addc9a7e85657f0db9f8
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2024-01-18 23:13:06 -0600

    feat: setup routes and livestream page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './app';
+import Livestream from './components/routes/livestream/livestream';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter( [
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/home',
+    element: <App />,
+  },
+  {
+    path: '/live-stream',
+    element: <Livestream />,
+  }
+] );
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
